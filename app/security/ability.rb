1699af6a7a0ce86149ed324d02bd3bacae40c37d
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-04-30 22:17:21 +0200

    Improved permissions depending on belonging to organization

--- app/security/ability.rb
+++ app/security/ability.rb
@@ -9,32 +9,33 @@
       can :manage, :all
       can :admin
     elsif user.roles.count > 0
-      can :manage, City
-      cannot [:create, :update, :destroy], City do |city|
-        !user.role? :curator, city
-      end
+      can [:read], City
+      # can :manage, City
+      # cannot [:create, :update, :destroy], City do |city|
+      #   !user.role? :curator, city
+      # end
       can [:read, :new, :search, :in_cities], RouteProfile
       can [:create], RouteProfile do |profile|
         user.role? :curator, profile.city
       end
       can [:update, :destroy, :crop], RouteProfile do |profile|
-        user.organization == profile.organization
+        (user.role? :curator, profile.city) && user.organization == profile.organization
       end
       can [:read, :new, :search, :waypoints], Route
       can [:create, :update, :destroy, :crop, :publish, :unpublish], Route do |route|
-        user.role? :curator, route.city && user.organization == route.route_profile.organization
+        (user.role? :curator, route.city) && user.organization == route.route_profile.organization
       end
       can [:read, :new, :search], Location
       can [:create], Location do |location|
         user.role? :curator, location.network
       end
       can [:update, :destroy, :crop], Location do |location|
-        user.role? :curator, location.network # && user.organization == location.organization
+        (user.role? :curator, location.network) #&& user.organization == location.organization  #TODO: location has no organization for now
       end
       #can :manage, Reward
 
       can [:new,:create, :update, :destroy], Reward do |reward|
-        user.role? :curator, reward.route.city && user.organization == reward.route.route_profile.organization
+        (user.role? :curator, reward.route.city) && user.organization == reward.route.route_profile.organization
       end
 
     else
